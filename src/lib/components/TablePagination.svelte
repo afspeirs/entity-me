53868c59664a7597f1ac788911f498d914ed0dac
andy.speirs <andy.speirs@havas.com>
2024-01-17 14:53:13 +0000

    fix: hide pagination when no search results are found

--- src/lib/components/TablePagination.svelte
+++ src/lib/components/TablePagination.svelte
@@ -19,31 +19,33 @@
   }
 </script>
 
-<nav
-  class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 select-none"
-  aria-label="Pagination"
->
-  <div class="hidden sm:block">
-    <p class="text-sm text-gray-700">
-      Showing <span class="font-medium">{start + 1}</span> to <span class="font-medium">{end + 1}</span> of <span class="font-medium">{totalItems}</span> results
-    </p>
-  </div>
-  <div class="flex flex-1 justify-between sm:justify-end">
-    <button
-      type="button"
-      on:click={handlePrevPage}
-      disabled={currentPage === 0}
-      class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0 disabled:opacity-40"
-    >
-      Previous
-    </button>
-    <button
-      type="button"
-      on:click={handleNextPage}
-      disabled={end + 1 === totalItems}
-      class="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0 disabled:opacity-40"
-    >
-      Next
-    </button>
-  </div>
-</nav>
+{#if totalItems > 0}
+  <nav
+    class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 select-none"
+    aria-label="Pagination"
+  >
+    <div class="hidden sm:block">
+      <p class="text-sm text-gray-700">
+        Showing <span class="font-medium">{start + 1}</span> to <span class="font-medium">{end + 1}</span> of <span class="font-medium">{totalItems}</span> results
+      </p>
+    </div>
+    <div class="flex flex-1 justify-between sm:justify-end">
+      <button
+        type="button"
+        on:click={handlePrevPage}
+        disabled={currentPage === 0}
+        class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0 disabled:opacity-40"
+      >
+        Previous
+      </button>
+      <button
+        type="button"
+        on:click={handleNextPage}
+        disabled={end + 1 === totalItems}
+        class="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0 disabled:opacity-40"
+      >
+        Next
+      </button>
+    </div>
+  </nav>
+{/if}
