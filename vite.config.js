06c35e9dad943d68c80fb59a63325ab0d4827cc5
andy.speirs <andy.speirs@havas.com>
2024-01-18 17:18:08 +0000

    feat: remove .version file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { readFileSync } from 'fs';
 import { defineConfig } from 'vite';
-
-const appVersion = readFileSync('./.version', { encoding: 'utf8' }).replace('\n', '') || 'N/A';
+import { version } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'import.meta.env.APP_VERSION': JSON.stringify(appVersion),
+    'import.meta.env.APP_VERSION': JSON.stringify(version || ''),
   },
   plugins: [
     svelte(),
