84698caddd509aa6d2925ba42bd3bc7060195a7b
Andy Speirs <andy.speirs@proseonpixels.com>
2024-09-16 15:18:15 +0100

    fix: force background colour of input

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -27,7 +27,7 @@
     id="search"
     bind:this={input}
     bind:value={$search}
-    class="block w-full rounded-none rounded-tl-md max-sm:rounded-tr-md sm:rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus-visible"
+    class="block w-full bg-white py-1.5 pl-10 text-gray-900 placeholder:text-gray-400 border-0 rounded-none rounded-tl-md max-sm:rounded-tr-md sm:rounded-l-md ring-1 ring-inset ring-gray-300 focus-visible"
     placeholder="Search entities"
   />
   <div class="absolute inset-y-0 right-0">
