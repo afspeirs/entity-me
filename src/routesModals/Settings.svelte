7340ded238ee4551d9259be1260eafc76c217166
Andy Speirs <andy.speirs@proseonpixels.com>
2024-10-14 12:50:07 +0100

    fix: update styling of settings modal

--- src/routesModals/Settings.svelte
+++ src/routesModals/Settings.svelte
@@ -42,30 +42,32 @@
 
 {#if $page.state.showModal === 'settings'}
   <Modal title="Settings" on:close={() => history.back()}>
-    <div class="flex rounded-md items-center w-full py-2 my-2 gap-3 text-sm text-gray-900 dark:text-white">
-      <Icon icon={`lucide:wrench`} class="size-5" aria-hidden="true" />
-      <span>Version:</span>
-      <span class="ml-auto">{import.meta.env.APP_VERSION}</span>
+    <div class="space-y-2">
+      <div class="flex rounded-md items-center w-full p-2 gap-2 text-sm text-gray-900 dark:text-white">
+        <Icon icon={`lucide:wrench`} class="size-5" aria-hidden="true" />
+        <span>Version</span>
+        <span class="ml-auto">{import.meta.env.APP_VERSION}</span>
+      </div>
+
+      <button
+        type="button"
+        on:click={handleUpdateServiceWorker}
+        class="flex rounded-md items-center w-full p-2 gap-2 text-sm hover:bg-black/5 dark:hover:bg-white/5 text-gray-900 dark:text-white"
+      >
+        <Icon icon={`lucide:rocket`} class="size-5" aria-hidden="true" />
+        <span>Check for update</span>
+        <span class="ml-auto">{$updateAvailable ? 'Update' : ''}</span>
+      </button>
+
+      <button
+        type="button"
+        on:click={handleUpdateTheme}
+        class="flex rounded-md items-center w-full p-2 gap-2 text-sm hover:bg-black/5 dark:hover:bg-white/5 text-gray-900 dark:text-white"
+      >
+        <Icon icon={`lucide:${$themeSystem === 'dark' ? 'moon' : 'sun'}`} class="size-5" aria-hidden="true" />
+        <span>Theme</span>
+        <span class="ml-auto">{themeUserOptions[$themeSystem]}</span>
+      </button>
     </div>
-
-    <button
-      type="button"
-      on:click={handleUpdateServiceWorker}
-      class="flex rounded-md items-center w-full py-2 my-2 gap-3 text-sm hover:bg-primary text-gray-900 hover:text-white dark:text-white"
-    >
-      <Icon icon={`lucide:rocket`} class="size-5" aria-hidden="true" />
-      <span>Check for update</span>
-      <span class="ml-auto">{$updateAvailable ? 'Update' : ''}</span>
-    </button>
-
-    <button
-      type="button"
-      on:click={handleUpdateTheme}
-      class="flex rounded-md items-center w-full py-2 my-2 gap-3 text-sm hover:bg-primary text-gray-900 hover:text-white dark:text-white"
-    >
-      <Icon icon={`lucide:${$themeSystem === 'dark' ? 'moon' : 'sun'}`} class="size-5" aria-hidden="true" />
-      <span>Theme:</span>
-      <span class="ml-auto">{themeUserOptions[$themeSystem]}</span>
-    </button>
   </Modal>
 {/if}
