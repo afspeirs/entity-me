0dc10e5eba5502d6823b8becbf19ca2be10131de
Andy Speirs <andy.speirs@proseonpixels.com>
2024-10-16 13:42:46 +0100

    feat: add spinner to indicate it is checking for an update

--- src/routesModals/Settings.svelte
+++ src/routesModals/Settings.svelte
@@ -7,6 +7,8 @@
   import { updateAvailable } from '$lib/stores/service-worker';
   import { themeSystem, themeUser, themeUserOptions } from '$lib/stores/theme';
 
+  let loading = false;
+
   // TODO: Refactor this into a store of modals to open and close the correct one
   function openSettings() {
     pushState('', {
@@ -14,15 +16,21 @@
     });
   }
 
-  function handleUpdateServiceWorker() {
+  function updateServiceWorker() {
+    if ('serviceWorker' in window.navigator) {
+      window.navigator.serviceWorker.ready.then((registration) => registration.update());
+    } else {
+      setTimeout(() => window.location.reload(), 1500);
+    }
+  }
+
+  function handleCheckForUpdate() {
     if ($updateAvailable) {
       $updateAvailable();
-    } else {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then((registration) => registration.update());
-      } else {
-        setTimeout(() => window.location.reload(), 1500);
-      }
+    } else if (!loading) {
+      loading = true;
+      updateServiceWorker();
+      setTimeout(() => (loading = false), 2000);
     }
   }
 
@@ -44,19 +52,32 @@
   <Modal title="Settings" on:close={() => history.back()}>
     <div class="space-y-2">
       <div class="flex rounded-md items-center w-full p-2 gap-2 text-sm text-gray-900 dark:text-white">
-        <Icon icon={`lucide:wrench`} class="size-5" aria-hidden="true" />
+        <Icon icon="lucide:wrench" class="size-5" aria-hidden="true" />
         <span>Version</span>
         <span class="ml-auto">{import.meta.env.APP_VERSION}</span>
       </div>
 
       <button
         type="button"
-        on:click={handleUpdateServiceWorker}
+        on:click={handleCheckForUpdate}
         class="flex rounded-md items-center w-full p-2 gap-2 text-sm hover:bg-black/5 dark:hover:bg-white/5 text-gray-900 dark:text-white"
       >
-        <Icon icon={`lucide:rocket`} class="size-5" aria-hidden="true" />
+        <Icon icon="lucide:rocket" class="size-5" aria-hidden="true" />
         <span>Check for update</span>
-        <span class="ml-auto">{$updateAvailable ? 'Update' : ''}</span>
+        <span class="ml-auto">
+          {#if $updateAvailable}
+            Update
+          {:else}
+            <div
+              aria-busy={loading}
+              aria-live="polite"
+              class:hidden={!loading}
+            >
+              <Icon icon="lucide:refresh-cw" class="size-5 animate-spin text-gray-900 dark:text-white" aria-hidden="true" />
+              <span class="sr-only">Loading</span>
+            </div>
+          {/if}
+        </span>
       </button>
 
       <button
