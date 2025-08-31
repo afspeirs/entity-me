3e6d08dec7742551d353465cda8d105b72c12646
Andrew Speirs <andrew@speirs.dev>
2025-08-31 16:59:44 +0100

    fix: redirect to root page

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = ({ url: { pathname } }) => {
+  if (pathname === '/') return;
+
+  redirect(303, '/');
+};
+
 export const prerender = true;
 export const ssr = false;
