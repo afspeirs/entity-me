0b269ae54bfdcc123ac6ac2ec29f51154ba5ec33
Andrew Speirs <andrew@speirs.dev>
2025-10-18 17:40:02 +0100

    fix: use await again to get the table component working nicely again

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -23,13 +23,9 @@
 
   let paginatedEntities: Entity[] = $state([]);
   const entitiesPromise = $derived(getEntities(currentCategory.value));
-  const maxColumns = headings.length;
-
-  let filteredEntities: Entity[] = $state([]);
-
-  $effect(() => {
-    entitiesPromise.then((items) => {
-      filteredEntities = items.filter(
+  const filteredItemsPromise = $derived(
+    entitiesPromise.then((items) =>
+      items.filter(
         (item) =>
           item.character?.includes(search.value) ||
           item.decimal?.includes(search.value) ||
@@ -37,9 +33,10 @@
           item.entity?.includes(search.value) ||
           item.description?.toLowerCase()?.includes(search.value.toLowerCase()) ||
           item.note?.toLowerCase()?.includes(search.value.toLowerCase()),
-      );
-    });
-  });
+      ),
+    ),
+  );
+  const maxColumns = headings.length;
 </script>
 
 <div class="flow-root min-w-full align-middle px-safe pb-safe">
@@ -53,17 +50,16 @@
         </tr>
       </thead>
       <tbody class="divide-y divide-gray-200 bg-white dark:bg-dark">
-        {#if hiddenColumns.value.length === maxColumns}
-          <tr>
-            <TableCell colspan={maxColumns}>Error: All columns are hidden</TableCell>
-          </tr>
-        {/if}
-        {#await entitiesPromise}
+        {#await filteredItemsPromise}
           <tr>
             <TableCell colspan={maxColumns}>Loading...</TableCell>
           </tr>
-        {:then}
-          {#if filteredEntities.length === 0}
+        {:then filteredItems}
+          {#if hiddenColumns.value.length === maxColumns}
+            <tr>
+              <TableCell colspan={maxColumns}>Error: All columns are hidden</TableCell>
+            </tr>
+          {:else if filteredItems.length === 0}
             <tr>
               <TableCell colspan={maxColumns}>
                 {#if currentCategory.value === 'favourites' && search.value.length === 0}
@@ -73,65 +69,68 @@
                 {/if}
               </TableCell>
             </tr>
+          {:else}
+            {#each paginatedEntities as entity, entityIdx (entity.decimal)}
+              <tr class="border-t {entityIdx === 0 ? 'border-gray-300' : 'border-gray-200'}">
+                <TableCell
+                  column="character"
+                  hidden={hiddenColumns.value.includes('character')}
+                  label={entity.character}
+                />
+                <TableCell
+                  column="decimal"
+                  hidden={hiddenColumns.value.includes('decimal')}
+                  label={`&#${entity.decimal};`}
+                />
+                <TableCell
+                  column="hex"
+                  hidden={hiddenColumns.value.includes('hex')}
+                  label={`&#x${entity.hex.padStart(4, '0')};`}
+                />
+                <TableCell
+                  column="entity"
+                  hidden={hiddenColumns.value.includes('entity')}
+                  label={entity.entity}
+                />
+                <TableCell
+                  column="description"
+                  hidden={hiddenColumns.value.includes('description')}
+                  label={entity.description}
+                />
+                <TableCell
+                  column="note"
+                  hidden={hiddenColumns.value.includes('note')}
+                  label={entity.note}
+                />
+                <TableCell column="favourite" hidden={hiddenColumns.value.includes('favourite')}>
+                  {@const favourite = favouriteEntities.value.includes(entity.description)}
+
+                  {#if favourite}
+                    <HeartIcon class="size-5 text-primary *:fill-primary" aria-hidden="true" />
+                  {:else}
+                    <HeartOffIcon class="size-5 text-primary" aria-hidden="true" />
+                  {/if}
+
+                  <button
+                    type="button"
+                    class="absolute inset-0 hover:bg-black/5 dark:hover:bg-white/5 cursor-pointer focus-outline-inset"
+                    onclick={() => favouriteEntities.update(entity.description)}
+                  >
+                    <span class="sr-only">{favourite ? 'Favourite' : 'Not a favourite'}</span>
+                  </button>
+                </TableCell>
+              </tr>
+            {/each}
           {/if}
-          {#each paginatedEntities as entity, entityIdx (entity.description)}
-            <tr class="border-t {entityIdx === 0 ? 'border-gray-300' : 'border-gray-200'}">
-              <TableCell
-                column="character"
-                hidden={hiddenColumns.value.includes('character')}
-                label={entity.character}
-              />
-              <TableCell
-                column="decimal"
-                hidden={hiddenColumns.value.includes('decimal')}
-                label={`&#${entity.decimal};`}
-              />
-              <TableCell
-                column="hex"
-                hidden={hiddenColumns.value.includes('hex')}
-                label={`&#x${entity.hex.padStart(4, '0')};`}
-              />
-              <TableCell
-                column="entity"
-                hidden={hiddenColumns.value.includes('entity')}
-                label={entity.entity}
-              />
-              <TableCell
-                column="description"
-                hidden={hiddenColumns.value.includes('description')}
-                label={entity.description}
-              />
-              <TableCell
-                column="note"
-                hidden={hiddenColumns.value.includes('note')}
-                label={entity.note}
-              />
-              <TableCell column="favourite" hidden={hiddenColumns.value.includes('favourite')}>
-                {@const favourite = favouriteEntities.value.includes(entity.description)}
-
-                {#if favourite}
-                  <HeartIcon class="size-5 text-primary *:fill-primary" aria-hidden="true" />
-                {:else}
-                  <HeartOffIcon class="size-5 text-primary" aria-hidden="true" />
-                {/if}
-
-                <button
-                  type="button"
-                  class="absolute inset-0 hover:bg-black/5 dark:hover:bg-white/5 cursor-pointer focus-outline-inset"
-                  onclick={() => favouriteEntities.update(entity.description)}
-                >
-                  <span class="sr-only">{favourite ? 'Favourite' : 'Not a favourite'}</span>
-                </button>
-              </TableCell>
-            </tr>
-          {/each}
         {:catch error}
           <tr>
-            <TableCell colspan={6}>Something went wrong: {error.message}</TableCell>
+            <TableCell colspan={maxColumns}>Something went wrong: {error.message}</TableCell>
           </tr>
         {/await}
       </tbody>
     </table>
-    <TablePagination items={filteredEntities} bind:trimmedData={paginatedEntities} />
+    {#await filteredItemsPromise then filteredItems}
+      <TablePagination items={filteredItems} bind:trimmedData={paginatedEntities} />
+    {/await}
   </div>
 </div>
